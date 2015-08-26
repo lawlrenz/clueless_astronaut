85bb502dc1fcd3bf3fd1cbe20a80d6965c9ca03f
Lorenz Heiler <contact@lorenzheiler.com>
2015-08-26 23:58:20 +0200

    somestuff

---
+++ Cluessless Astronaut 2D/Assets/Scripts/playerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	public float jumpHeight;
+	public float movementSpeed;
+
+	public bool isSpacePressed;
+	public bool isRightArrowPressed;
+	public bool isLeftArrowPressed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			isSpacePressed = true;
+		} else {
+			isSpacePressed = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			isRightArrowPressed = true;
+		} else {
+			isRightArrowPressed = false;
+		}
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			isLeftArrowPressed = true;
+		} else {
+			isLeftArrowPressed = false;
+		}
+	}
+	void FixedUpdate() {
+		if(isSpacePressed){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+		if (isRightArrowPressed) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (isLeftArrowPressed) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
