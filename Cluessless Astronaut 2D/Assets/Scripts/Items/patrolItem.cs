57a7b74bc2fa387b0e22ef8bb3698636e598cb75
donfussl <s6chvoss@uni-bonn.de>
2015-08-28 00:25:04 +0200

    level 4 spielerei

---
+++ Cluessless Astronaut 2D/Assets/Scripts/Items/patrolItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class patrolItem : MonoBehaviour {
+
+	private float xLeft;
+	private float xRight;
+	private float yLeft;
+	private float yRight;
+	private float movSpeed;
+
+	private Vector2 startingPos;
+
+	// Use this for initialization
+	void Start () {
+		startingPos.x = this.transform.x;
+		startingPos.y = this.transform.y;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
