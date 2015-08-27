9f13d42ae66cd06b843c4efdf479a1edf6660fa7
donfussl <s6chvoss@uni-bonn.de>
2015-08-27 17:56:01 +0200

    level added

---
+++ Cluessless Astronaut 2D/Assets/Scripts/level_select_screen/load_level.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class load_level : MonoBehaviour {
+
+	public int levelID;
+	private bool mouseOver;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mouseOver) {
+			if (Input.GetMouseButtonDown(0)){
+				Application.LoadLevel(levelID);
+			}
+		}
+	}
+	
+	void OnMouseEnter(){
+		mouseOver = true;
+	}
+	
+	void OnMouseExit(){
+		mouseOver = false;
+	}
+	
+}
