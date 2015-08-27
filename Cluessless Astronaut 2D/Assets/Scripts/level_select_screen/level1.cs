945c30fe1a37c3d554ea0424f4a3865135541f3d
donfussl <s6chvoss@uni-bonn.de>
2015-08-27 17:33:48 +0200

    stuff

---
+++ Cluessless Astronaut 2D/Assets/Scripts/level_select_screen/level1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level1 : MonoBehaviour {
+	
+	private bool mouseOver;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mouseOver) {
+			if (Input.GetMouseButtonDown(0)){
+				Application.LoadLevel(5);
+			}
+		}
+	}
+	
+	void OnMouseEnter(){
+		mouseOver = true;
+	}
+	
+	void OnMouseExit(){
+		mouseOver = false;
+	}
+	
+}
