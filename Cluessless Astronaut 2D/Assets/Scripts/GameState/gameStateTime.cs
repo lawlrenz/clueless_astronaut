f7529a2a8998b54c849218a17cc6d42b58b28f23
Lorenz Heiler <contact@lorenzheiler.com>
2015-08-27 16:59:14 +0200

    Scores and LevelExit
    epic.

--- Cluessless Astronaut 2D/Assets/Scripts/GameState/gameStateTime.cs
+++ Cluessless Astronaut 2D/Assets/Scripts/GameState/gameStateTime.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 public class gameStateTime : MonoBehaviour {
 	Text currTime;
 	
-	//GameObject target = GameObject.Find("Time");
-
 	// Use this for initialization
 	void Start () {
 		PlayerPrefs.SetFloat("LevelStartTime", Time.realtimeSinceStartup);
@@ -16,5 +14,6 @@ public class gameStateTime : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		currTime.text = (Time.realtimeSinceStartup - PlayerPrefs.GetFloat("LevelStartTime")).ToString("#.00");
+		PlayerPrefs.SetFloat ("CurrTime",(Time.realtimeSinceStartup - PlayerPrefs.GetFloat("LevelStartTime")));
 	}
 }
