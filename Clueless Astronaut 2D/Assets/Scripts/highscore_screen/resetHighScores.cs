f9ded78908b155edecb37aa6f35a0095a14d633d
Lorenz Heiler <contact@lorenzheiler.com>
2015-08-28 12:03:58 +0200

    Highscore reset + dynamic

---
+++ Clueless Astronaut 2D/Assets/Scripts/highscore_screen/resetHighScores.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resetHighScores : MonoBehaviour {
+	private TextMesh textm;
+    private bool mouseOver;
+	public int levelamount;
+	int sceneoffset = 4;
+
+	// Use this for initialization
+	void Start () {
+		textm = GetComponent<TextMesh> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mouseOver) {
+			if (Input.GetMouseButtonDown(0)){
+				for(int i = 1; i <= levelamount; i++){
+					
+					int scene_iter = i + sceneoffset;
+					string timeident = scene_iter + "LevelTime";
+					
+					PlayerPrefs.SetFloat(timeident, 999.99f);
+				}			
+			}
+		}
+	}
+	void OnMouseEnter(){
+		if (textm != null) {
+			textm.color = new Color (0, 255, 0);
+		}
+		mouseOver = true;
+	}
+	
+	void OnMouseExit(){
+		if (textm != null) {
+			textm.color = new Color (255, 255, 255);
+		}
+		mouseOver = false;
+	}
+
+}
--- Clueless Astronaut 2D/Assets/Scripts/highscore_screen/viewHighScores.cs
+++ Clueless Astronaut 2D/Assets/Scripts/highscore_screen/viewHighScores.cs
@@ -7,20 +7,20 @@ public class viewHighScores : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject scoreboard = GameObject.Find ("scoreboard");
-		TextMesh scoreboard_mesh = (TextMesh) scoreboard.GetComponent(typeof(TextMesh));
 
-		for(int i = 1; i <= levelamount; i++){
-
-			int scene_iter = i + sceneoffset;
-			string timeident = scene_iter + "LevelTime";
-
-			scoreboard_mesh.text += "\n#" + i + " - " + PlayerPrefs.GetFloat(timeident).ToString("#.00");
-		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameObject scoreboard = GameObject.Find ("scoreboard");
+		TextMesh scoreboard_mesh = (TextMesh) scoreboard.GetComponent(typeof(TextMesh));
+		scoreboard_mesh.text = "";
+		for(int i = 1; i <= levelamount; i++){
+			
+			int scene_iter = i + sceneoffset;
+			string timeident = scene_iter + "LevelTime";
+			
+			scoreboard_mesh.text += "\n#" + i + " - " + PlayerPrefs.GetFloat(timeident).ToString("#.00");
+		}
 	}
 }
