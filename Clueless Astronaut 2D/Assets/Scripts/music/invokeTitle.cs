00e9264133faa0e6424a8fa1d94b507adef512ad
donfussl <s6chvoss@uni-bonn.de>
2015-08-28 19:05:37 +0200

    musichandler finished

--- Clueless Astronaut 2D/Assets/Scripts/music/invokeTitle.cs
+++ Clueless Astronaut 2D/Assets/Scripts/music/invokeTitle.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class invokeTitle : MonoBehaviour {
 
 	public AudioClip ac;
+	public float volume;
 
 	// Use this for initialization
 	void Start () {
 		GameObject go = GameObject.Find ("bgMusicHandler");
-		go.GetComponent<AudioSource> ().clip = ac;
-		go.GetComponent<AudioSource> ().Play ();
-
+		if (go.GetComponent<AudioSource> ().clip != ac) {
+			go.GetComponent<AudioSource> ().clip = ac;
+			go.GetComponent<AudioSource> ().volume = volume;
+			go.GetComponent<AudioSource> ().loop = true;
+			go.GetComponent<AudioSource> ().Play ();
+		}
 	}
 	
 	// Update is called once per frame
