7a7606c97327bb042449cabea93d8fd63e374633
donfussl <s6chvoss@uni-bonn.de>
2015-08-28 08:41:20 +0200

    stuff

--- Cluessless Astronaut 2D/Assets/Scripts/Items/patrolItem.cs
+++ Cluessless Astronaut 2D/Assets/Scripts/Items/patrolItem.cs
@@ -38,7 +38,6 @@ public class patrolItem : MonoBehaviour {
 		linearKomb += mod * movSpeed * 0.01f;
 		float newPosX = linearKomb * 0.01f * p1.x + (1 - linearKomb * 0.01f) * p2.x;
 		float newPosY = linearKomb * 0.01f * p1.y + (1 - linearKomb * 0.01f) * p2.y;
-		//this.transform.position.x = linearKomb * 0.01f * p1.x + (1 - linearKomb * 0.01f) * p2.x;
 		this.transform.position = new Vector3 (newPosX, newPosY, 0);
 	}
 
