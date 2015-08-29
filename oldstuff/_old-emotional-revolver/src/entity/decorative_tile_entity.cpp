0139d832d84292e86aabae3b128e44b977ec0f27
Lorenz Heiler <contact@lorenzheiler.com>
2015-08-29 12:03:54 +0200

    licensing stuff und aufräumen

--- oldstuff/_old-emotional-revolver/src/entity/decorative_tile_entity.cpp
+++
@@ -1,7 +0,0 @@
-#include "decorative_tile_entity.h"
-
-DecorativeTileEntity::DecorativeTileEntity(int subtype):
-	TileEntity(ENTITY_TYPE_DECORATIVE_TILE, subtype)
-{
-
-}
--- oldstuff/_old-emotional-revolver/src/entity/solid_tile_entity.cpp
+++
@@ -1,7 +0,0 @@
-#include "solid_tile_entity.h"
-
-SolidTileEntity::SolidTileEntity(int subtype):
-	TileEntity(ENTITY_TYPE_TILE, subtype)
-{
-
-}
